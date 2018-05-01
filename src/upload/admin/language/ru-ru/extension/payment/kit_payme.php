7f44fbe6204861ceed1a4437f8ac990077831295
umidjons <almatov.us@gmail.com>
2018-05-01 10:57:03 +0500

    Translations corrected. Split source and installable archive.

---
+++ src/upload/admin/language/ru-ru/extension/payment/kit_payme.php
@@ -0,0 +1,40 @@
+<?php
+// Heading
+$_['heading_title'] = 'KiT Payme';
+
+// Text
+$_['text_extension'] = 'Расширения';
+$_['text_success']   = 'Успешно: Вы успешно изменили параметры!';
+$_['text_edit']      = 'Настройте параметры оплаты';
+$_['text_kit_payme'] = '<a target="_BLANK" href="https://business.payme.uz/"><img style="width: 100px;" src="view/image/payment/kit_payme.png" alt="Kit Payme" title="Kit Payme" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_live']      = 'Боевой';
+$_['text_sandbox']   = 'Тестовый';
+
+// Entry
+$_['entry_kit_payme_merchant_id']       = 'ID поставщика';
+$_['entry_kit_payme_redirect']          = 'Перенаправление URL';
+$_['entry_merchant_private_key']        = 'Ключ - пароль кассы';
+$_['entry_merchant_private_key_test']   = 'Ключ - тестовый пароль кассы';
+$_['entry_kit_payme_mob']               = 'Url для Payme:';
+$_['entry_status']                      = 'Состояние';
+$_['entry_enabled']                     = 'Включить режим тестирования';
+$_['entry_kit_payme_checkout_url']      = 'Введите URL-адрес шлюза';
+$_['entry_kit_payme_checkout_url_test'] = 'Введите тестовый URL-адрес шлюза';
+
+$_['entry_kit_payme_callback_pay'] = 'Вернуться после оплаты через';
+$_['entry_kit_payme_status_tovar'] = 'Добавить в чек данные о товарах';
+$_['entry_kit_payme_endpoint_url'] = 'Данные URL необходимо внести в настройке кассы в кабинете поставщика';
+$_['entry_kit_payme_description']  = 'Настройки для кассы можно получить с кабинета <a href="https://paycom.uz/">поставщика</a> Раздел Параметры для разработчика';
+
+// Help
+$_['help_total']        = 'The checkout total the order must reach before this payment method becomes active';
+$_['help_alipay_setup'] = '<a target="_blank" href="https://github.com/PaycomUZ/opencart-payment-gateway/blob/master/README.md">Здесь</a> находиться подробная описания.';
+
+// Error
+$_['error_permission']                  = 'Предупреждения: У вас недостаточно привилегий изменить оплату!';
+$_['error_kit_payme_merchant_id']       = 'ID поставщика объязателен!';
+$_['error_merchant_private_key']        = 'Ключ кассы объязателен!';
+$_['error_merchant_private_key_test']   = 'Тестовый ключ кассы объязателен!';
+$_['error_kit_payme_checkout_url']      = 'Установите Paycom Checkout URL для подтверждения оплаты';
+$_['error_kit_payme_checkout_url_test'] = 'Установите тестовый Paycom Checkout URL для подтверждения оплаты';
+$_['error_kit_payme_mob_payme']         = 'Url для Payme';
